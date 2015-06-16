faa6e0df286c1e8bb5b335c746b60f9f56999d32
Josinaldo Batista <josinaldo.batista@hotmail.com>
2015-06-16 08:15:01 -0300

    Revert "giselly obesa"
    This reverts commit 315e7fc5462ae93966bae3abd9a09a87158fff58.

--- Classes/Config.inc.php
+++ Classes/Config.inc.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 // CONFIGRAÇÕES DO SITE ####################
@@ -25,4 +26,33 @@ function __autoload($Class) {
         trigger_error("Não foi possível incluir {$Class}.class.php", E_USER_ERROR);
         die;
     }
+=======
+<?php
+
+// CONFIGRAÇÕES DO SITE ####################
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', 'vertrigo');
+define('DBSA', 'preavaliacao');
+define('RAIZ', 'http://10.100.10.91/preAvaliacaoWEB/');
+
+// AUTO LOAD DE CLASSES ####################/
+function __autoload($Class) {
+
+    $cDir = array('DAO', 'Business', 'Entity', 'Exception');
+
+    $iDir = null;
+
+    foreach ($cDir as $dirName){
+        if (!$iDir && file_exists(__DIR__."/{$dirName}/{$Class}.class.php")){
+            include_once (__DIR__."/{$dirName}/{$Class}.class.php");
+            $iDir = true;
+        }
+    }
+
+    if (!$iDir){
+        trigger_error("Não foi possível incluir {$Class}.class.php", E_USER_ERROR);
+        die;
+    }
+>>>>>>> origin/master
 }
