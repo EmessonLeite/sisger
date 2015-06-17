c9055394eb7197d84f0d6555c29b1740f1eb2e74
Ruan Nícolas <nicolasmatos0905@gmail.com>
2015-06-17 08:56:50 -0300

    ...

--- js/home.js
+++ js/home.js
@@ -1,24 +1,36 @@
 $(document).ready(function () {
-    /** Valor atual do atributo css top da div#lista-pessoas   */
+    /**
+     * Valor atual do atributo css top da div#lista-pessoas
+     */
     var top = (-1) * $('#top').val();
 
     $('#lista-pessoas').css('top', top + 'px');
 
-    /** Altura minima que a div#lista-pessoas pode ter */
+    /**
+     * Altura minima que a div#lista-pessoas pode ter
+     */
     var min = ($('#lista-pessoas').height() - $('#listagem-geral').height()) * (-1);
 
-    /** Altura max que a div#lista-pessoas pode ter */
+    /**
+     * Altura max que a div#lista-pessoas pode ter
+     */
     var max = 0;
 
-    /** Direção atual da animação. Pode ser: parado, cima, baixo */
+    /**
+     * Direção atual da animação. Pode ser: parado, cima, baixo
+     */
     var direcao = "parado";
 
-    /** Redireciona para o funcionario clicado */
+    /**
+     * Redireciona para o funcionario clicado
+     */
     $('.funcionarios').click(function () {
         $(this).attr('href', $(this).attr('href') + '/' + ((-1) * top));
     });
 
-    /** Redireciona para a avaliacao clicada */
+    /**
+     * Redireciona para a avaliacao clicada
+     */
     $('select#avaliacoes option').click(function () {
         if ($(this).val() !== "0") {
             window.location = "home/" + $(this).html() + "/" + $('#idSelecionado').val() + "/0";
@@ -40,7 +52,6 @@ $(document).ready(function () {
     $("a[rel*=leanModal]").leanModal({top: 200, overlay: 0.4, closeButton: ".modal_close"});
 
     /**
-     * 
      * 
      */
     $('.frmComentarios').submit(function () {
@@ -68,7 +79,7 @@ $(document).ready(function () {
         return false;
     });
 
-    /*
+    /**
      * Marcar aba clicada com uma cor diferente, indicando que esta está selecionada
      */
     $('.info').click(function () {
@@ -101,7 +112,7 @@ $(document).ready(function () {
         }
     });
 
-    /*
+    /**
      * Pausa a barra de rolagem para cima da listagem de pessoas
      */
     $('#setaBaixo').mouseup(function () {
@@ -125,7 +136,7 @@ $(document).ready(function () {
         }
     });
 
-    /*
+    /**
      * Pausa a barra de rolagem para baixo da listagem de pessoas
      */
     $('#setaCima').mouseup(function () {
@@ -134,7 +145,7 @@ $(document).ready(function () {
 
 });
 
-/*
+/**
  * Função que muda a cor da aba que esta selecionada
  */
 function mudarCor(btnSelecionado) {
