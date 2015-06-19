fe9f8dc1570911ff45741d749b28a9dfa43c9542
Josinaldo Batista <josinaldo.batista@hotmail.com>
2015-06-19 10:20:13 -0300

    Redefinir senha 1.1

--- includes/increcuperaSenha.php
+++ includes/increcuperaSenha.php
@@ -1,3 +1,39 @@
 <?php
 
+$info = '';
+/**
+ * Inclui classes de envio de email
+ */
+require_once('Classes/mail/class.phpmailer.php');
+require_once('pages/email.php');
+
+/**
+ * Verifica se foi enviado via POST o form
+ */
+if (isset($_POST['enviar']) && is_string($_POST['enviar'])) {
+    $nome = $_POST['usuario'];
+    $email = $_POST['email'];
+    $dados = validaUsuario($nome, $email);
+    $nomeRemetente = 'Instituto Tecnológico e Vocacional Avançado';
+    $remetente = 'iteva@iteva.org.br';
+
+    if ($dados) {
+        $id = $dados->id;
+        $data = strtotime(date("Y-m-d H:i:s"));
+        $parametro = base64_encode($data . $id);
+
+        $mensagem = "Você pediu para recuperar a senha da pre-avaliação.<br/>
+                     Click no link abaixo e redefina sua senha<br/>
+                    <a href='" . RAIZ . "redefine/{$parametro}'>Redefinir senha.</a>
+        ";
+        sendMail('Recuperar Senha', $mensagem, $remetente, $nomeRemetente, $email, $nome, '', '');
+
+        $info = "Foi enviado um link pro seu email.";
+        echo "<meta http-equiv='refresh' content=5;url='" . RAIZ . "login'>";
+    } else {
+        $info = "Nome ou email não cadastrado.";
+    }
+}
+$info = ($info != '') ? "<p id='info'>{$info}</p>" : '';
+
 include_once('pages/pgrecuperaSenha.php');
