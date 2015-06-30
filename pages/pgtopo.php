578fb8f28214690359617bec2c5192f2bcff96c1
Ruan Nícolas <nicolasmatos0905@gmail.com>
2015-06-30 11:28:27 -0300

    Drop para configuração 1.4

--- pages/pgtopo.php
+++ pages/pgtopo.php
@@ -31,35 +31,35 @@
                 </div>
                 <div id="icons">
                     <a><img id="config" src="imagens/config.png"></a>
+                    <a href="logout"> <img src="imagens/sair.png"></a>
                     <div id="drop-config">
                         <img id="seta-cima" width="15" height="10" src="imagens/seta-cima-config.png">
-                        <a href="alterarLogin"><p style="padding-top: 0">Alterar login</p></a>
-                        <a href="alterarSenha"><p style="padding-top: 0">Alterar senha</p></a>
+                        <a href="alterarLogin"><p id="alterarLogin">Alterar login</p></a>
+                        <a href="alterarSenha"><p>Alterar senha</p></a>
                         <?php
-                            /** Índice 1 no array de permissoes indica permissao de Gestão de funcionarios */
-                            if (isset($permissoes[1])) {
-                                echo '<a href="cadastraFunc"><p>Cadastrar funcionário</p></a>';
-                            }
+                        /** Índice 1 no array de permissoes indica permissao de Gestão de funcionarios */
+                        if (isset($permissoes[1])) {
+                            echo '<a href="cadastraFunc"><p id="permissao1">Cadastrar funcionário</p></a>';
+                        }
 
-                            /** Índice 2 no array de permissoes indica permissao de Gestão de cargos */
-                            if(isset($permissoes[2])){
-                                echo '<a href="cadastraCargo"><p>Cadastrar cargo</p></a>';
-                            }
+                        /** Índice 2 no array de permissoes indica permissao de Gestão de cargos */
+                        if(isset($permissoes[2])){
+                            echo '<a href="cadastraCargo"><p id="permissao2">Cadastrar cargo</p></a>';
+                        }
 
-                            /** Índice 3 no array de permissoes indica permissao de Gestão de avaliação */
-                            if(isset($permissoes[3])){
-                                echo '<a href="cadastraAva"><p>Cadastrar avaliação</p></a>';
-                            }
+                        /** Índice 3 no array de permissoes indica permissao de Gestão de avaliação */
+                        if(isset($permissoes[3])){
+                            echo '<a href="cadastraAva"><p id="permissao3">Cadastrar avaliação</p></a>';
+                        }
 
-                            /** Índice 4 no array de permissoes indica permissao de Gestão de Permissões */
-                            if(isset($permissoes[4])){
-                                echo '<a href="cadastraPermissoes"><p>Cadastrar Permissões</p></a>';
-                            }
+                        /** Índice 4 no array de permissoes indica permissao de Gestão de Permissões */
+                        if(isset($permissoes[4])){
+                            echo '<a href="cadastraPermissoes"><p id="permissao4">Cadastrar Permissões</p></a>';
+                        }
+
                         ?>
-
-
                     </div>
-                    <a href="logout"> <img src="imagens/sair.png"></a>
                 </div>
             </div>
         </div>
+
