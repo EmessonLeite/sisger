6d3a195dd65bd8a03a98ebf8253c88bcdbe31494
Josinaldo Batista <josinaldo.batista@hotmail.com>
2015-07-01 11:29:27 -0300

    Cadastrar novo funcionario 1.0

--- includes/inccadastraFunc.php
+++ includes/inccadastraFunc.php
@@ -6,10 +6,18 @@
  */
 Permissao::validar(isset($permissoes[1]));
 
+/** Include o formulario de cadastro/edição de funcionarios */
+if ($url->posicaoExiste(1) && ($url->getURL(1) == 'novo' || $url->getURL(1) == 'editar')) {
+    /** Include da pagina de configuração de perfil */
+    include_once("pages/pgForm{$url->getURL(0)}.php");
+    exit;
+}
+
 /** @var Usuario */
 $usuarioBusiness = Usuario::getInstance();
 
+/** @var array */
 $dadosUsuarios = $usuarioBusiness->buscar();
 
 /** Include da pagina de configuração de perfil */
-include_once('pages/pgcadastraFunc.php');
+include_once("pages/pg{$url->getURL(0)}.php");
---
+++ pages/pgFormcadastraFunc.php
@@ -0,0 +1,15 @@
+<section>
+    <form name="formUsuario">
+        <h1>Funcionario</h1>
+        
+        <input type="hidden" name="tipo" value="novo" />
+        
+        <label for="nome">Nome completo</label>
+        <input name="nome" value="" />
+        
+        <label for="apelido">Nome de Visualiazação</label>
+        <input name="apelido" value="" />
+        
+        <input type="submit" name="cadastrar" value="Salvar"> 
+    </form>
+</section>
